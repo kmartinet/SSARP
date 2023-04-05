8bc952d9beb9160a09a70e96c6623f9b26f1fb87
kmartinet <33140392+kmartinet@users.noreply.github.com>
2023-04-05 09:23:44 -0700

    findAreas is working better now, but still not perfect

--- R/Find_Areas.R
+++ R/Find_Areas.R
@@ -3,7 +3,7 @@
 #' Use various mapping tools to attempt to find the names of land masses where the occurrence points were found.
 #' @param occurrences The dataframe output by getData (or if using a custom dataframe, ensure that it has the following columns: decimalLongitude, decimalLatitude, acceptedScientificName)
 #' @param fillgaps (logical) Attempt to use Photon API to fill in gaps left by mapdata::map.where (TRUE) or only mapdata::map.where results (FALSE, default). While it is powerful, the Photon API does not have a standard location for island names in its returned information, so using it will likely require the returned dataframe to be cleaned by the user.
-#' @return A dataframe of the species name, longitude, latitude, country, and island of occurrence
+#' @return A dataframe of the species name, longitude, latitude, and three parts of occurrence information. "First" is the name used to describe the largest possible area of land where the occurrence point is found. "Second" is the name used to describe the second-largest possible area of land that corresponds with the occurrence point. "Third" is the most specific area of land that corresponds with the occurrence point. Functions later in the SSARP pipeline default to checking whether "Third" has an entry, then look at "Second," and then "First."
 #' @examples 
 #' \dontrun{occs <- findLand(occurrences, fillgaps = FALSE)}
 #' @import mapdata
@@ -16,14 +16,29 @@
 findLand <- function(occurrences, fillgaps = FALSE) {
   lon<-as.numeric(occurrences$decimalLongitude)
   lat<-as.numeric(occurrences$decimalLatitude)
-  # Use map.where to figure out what land mass the 
-  #where<-maps::map.where(database="world2Hires", x=lon, y=lat)
-  where<-maps::map.where(database="world", x=lon, y=lat)
+  # Use map.where to find landmass names that correspond to GPS points
+  # First, use world2Hires
+  where <- maps::map.where(database="mapdata::world2Hires", x=lon, y=lat)
+  # Then, use world just in case it can fill gaps
+  where2 <- maps::map.where(database="world", x=lon, y=lat)
   
-  occs <- as.data.frame(cbind(occurrences$acceptedScientificName, lon, lat, where))
+  # Next, combine the two where options (prioritize where2)
+  for(i in c(1:length(where2))) {
+    # Check if this row is NA
+    if(is.na(where2[i])){
+      # If it is NA, check if where is not NA
+      if(!is.na(where[i])) {
+        # If where is not NA, grab the name it found
+        where2[i] <- where[i]
+      }
+    }
+  }
+  
+  occs <- as.data.frame(cbind(occurrences$acceptedScientificName, lon, lat, where2))
   # Separate the where column into two separate columns - Country and Island
-  occs <- occs %>% tidyr::separate(where, c("Country", "Island"), sep = ":")
-  colnames(occs) <- c("Species", "Longitude", "Latitude", "Country", "Island")
+  # But sometimes there are three...
+  occs <- occs %>% tidyr::separate(where2, c("First", "Second", "Third"), sep = ":")
+  colnames(occs) <- c("Species", "Longitude", "Latitude", "First", "Second", "Third")
   
   if(fillgaps == TRUE) {
     # There might still be a lot of NA entries, so use Photon to try to fill in gaps
@@ -31,7 +46,7 @@ findLand <- function(occurrences, fillgaps = FALSE) {
     for(i in c(1:nrow(occs))) {
       if(is.na(occs[i,4])){
         # Get lon and lat
-        longitude = occs[i,2]
+        longitude = occs[i,4]
         latitude = occs[i,3]
         
         # Create Photon URL
@@ -53,7 +68,7 @@ findLand <- function(occurrences, fillgaps = FALSE) {
           
           # Next, try to fill in the island column
           # The "locality" part of the Photon data often has the island name
-          # If that part is empty, get the "county" part instead
+          # If that part is empty, get the "county" part instead - COUNTY COULD BE A PROBLEM
           if(length(PhotonInfo$locality) != 0) {
             occs[i,5] <- as.character(PhotonInfo$locality)
           } else if(length(PhotonInfo$county) != 0) {
@@ -74,16 +89,34 @@ findLand <- function(occurrences, fillgaps = FALSE) {
 }
 
 
-#' Find areas of land masses. CURRENTLY BROKEN?
+#' Find areas of land masses.
 #' 
 #' Reference a dataset of island names and areas to find the areas of the land masses relevant to the taxon of interest.
-#' @param occs The dataframe that is returned by SSARP::findLand. If using a custom dataframe, ensure that it has an "Island" column and a "Country" column.
+#' @param occs The dataframe that is returned by SSARP::findLand. If using a custom dataframe, ensure that it has the following columns in order: "Species", "Longitude", "Latitude", "First", "Second", "Third"
 #' @return A dataframe of the species name, island name, and island area
 #' @examples 
 #' \dontrun{areas <- findAreas(occs)}
 #' @export
 
 findAreas <- function(occs){
+  # Remove rows where First, Second, and Third are all NA
+  # Create vector to hold row numbers
+  minus <- rep(NA, nrow(occs))
+  # Loop through dataframe
+  for(i in c(1:nrow(occs))){
+    if(is.na(occs[i,6]) && is.na(occs[i,5]) && is.na(occs[i,4])) {
+      minus[i] <- i
+    }
+  }
+  # Remove NAs from row number vector
+  minus <- minus[!is.na(minus)]
+  
+  occs <- occs[-minus,]
+  
+  # Rename rows to make future loops make more sense
+  #num_rows <- nrow(occs)
+  #rownames(occs) <- c(1:num_rows)
+  
   # Add a temporary key-value pair to initialize
   IslandDict <- Dict$new(
     bloop = 108
@@ -94,14 +127,17 @@ findAreas <- function(occs){
   # First, create an empty list of island names
   islands <- list()
   
-  # Next, go through the occs dataframe and see if the island column has a name in it
-  # If yes, add to the island list. If NA, go to the country column.
+  # Next, go through the occs dataframe and see if the Third column has a name in it
+  # If yes, add to the island list. If NA, go to the Second column. If NA, go to the First column
   
   for(i in c(1:nrow(occs))) {
-    if(!is.na(occs[i,5])) {
+    if(!is.na(occs[i,6])) {
+      islands[i] <- occs[i,6]
+    }
+    else if (!is.na(occs[i,5])){
       islands[i] <- occs[i,5]
     }
-    else {
+    else if (!is.na(occs[i,4])){
       islands[i] <- occs[i,4]
     }
   }
@@ -110,9 +146,8 @@ findAreas <- function(occs){
   uniq_islands <- unique(islands)
   
   # Next, add the island names as keys and their corresponding areas as values
-  # CAN'T GET DATA TO WORK??
-  #area_file <- read.csv("data/EditedAllIslands.csv")
-  area_file <- SSARP::island_areas # Should already be loaded with the package??
+  # Get island areas from built-in area file
+  area_file <- SSARP::island_areas
   
   # Look through the island area file and find the names in the uniq_islands list
   for (i in c(1:length(uniq_islands))) {
@@ -129,6 +164,8 @@ findAreas <- function(occs){
       
       if(area_compare == uniq_compare) {
         #testing[islands[i]] <- areas[i]
+        print(as.character(uniq_islands[i]))
+        print(area_file[j,3])
         IslandDict[as.character(uniq_islands[i])] <- area_file[j,3]
         print("Found the island name for: ")
         print(i)
@@ -150,18 +187,23 @@ findAreas <- function(occs){
   areas <- rep(0, times = nrow(occs))
   
   for(i in c(1:nrow(occs))) {
-
-    if(is.na(occs[i,5])){
-      
-      if(!is.na(occs[i,4])) {
-        print(i)
-        areas[i]<-IslandDict$get(occs[i,4])
-      }
-    }
     
-    if(!is.na(occs[i,5])) {
-      areas[i] <- IslandDict$get(occs[i,5])
+    if(!is.na(occs[i,6]) && IslandDict$has(occs[i,6])){
+      print(i)
+      areas[i]<-IslandDict$get(occs[i,6])
     }
+    else if(!is.na(occs[i,5]) && IslandDict$has(occs[i,5])){
+      print(i)
+      areas[i]<-IslandDict$get(occs[i,5])
+    }
+    else if(!is.na(occs[i,4]) && IslandDict$has(occs[i,4])){
+      print(i)
+      areas[i]<-IslandDict$get(occs[i,4])
+    }
+    else {
+      areas[i]<-NA
+    }
+
   }
   
   # Create final dataframe
